979367c793fcbba6dd5ffe6e5c207b0adb165c97
thanhict <congthanh28924@gmail.com>
2021-12-08 22:13:32 +0700

    add

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
-const text = document.querySelector('.text');
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-let load = 0;
-let int = setInterval(blurring, 30);
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
 
-function blurring() {
-    load++;
-    if (load > 99) {
-        clearInterval(int);
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
-    text.innerHTML = load + '%';
-    text.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
