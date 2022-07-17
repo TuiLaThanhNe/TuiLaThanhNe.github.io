b190c8c2f5e7e9527863feaca5a324e7f9cd0ab5
Thành CgT <93459882+TuiLaThanhNe@users.noreply.github.com>
2022-07-17 21:17:25 +0700

    Add files via upload

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-music = ['ido', 'noinaycoanh', 'nguoiamphu', 'duong-toi-cho-em-ve-lofi-ver-buitruonglinh-x-freak-d', 'gap-go-yeu-duong-va-duoc-ben-em', 'nam-doi-ban-tay-kay-tran-official-music-video', 'nhung-ban-lofi-freak-d-mix-ver-1.0', 'the-playah-ballad-soobin-hoang-son-ft-slimv-special-version-1-hour'];
+music = ['loinho', 'noinaycoanh', 'nguoiamphu','nhuanhmo','coem', 'anhsaovabautroi', 'bluetequila', 'bainaychillphet', 'Rapcoustic5', 'the-playah-ballad-soobin-hoang-son-ft-slimv-special-version-1-hour'];
 
 // Thay đổi link ramdom bài hát tại đây
 document.querySelector("audio").setAttribute("src", `./love/music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
